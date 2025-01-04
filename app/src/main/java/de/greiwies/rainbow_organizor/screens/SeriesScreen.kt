32023113f898e959f2ff7284219fd1d882992959
Lithinem <77071017+Lithinem@users.noreply.github.com>
2025-01-04 11:10:41 +0100

    Beautified Series Screen

--- app/src/main/java/de/greiwies/rainbow_organizor/screens/SeriesScreen.kt
+++ app/src/main/java/de/greiwies/rainbow_organizor/screens/SeriesScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -20,10 +21,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import de.greiwies.rainbow_organizor.DataEntry
+import de.greiwies.rainbow_organizor.R
 import de.greiwies.rainbow_organizor.RainbowViewModel
 import de.greiwies.rainbow_organizor.ui.theme.OverlayBackgroundGrayHalfTransparent
 
@@ -40,8 +44,14 @@ fun DetailsScreen(viewModel: RainbowViewModel) {
 
 @Composable
 fun DataEntryGrid(entries: List<DataEntry>) {
+    val context = LocalContext.current
+    val tileSizeInDp = with(LocalDensity.current) {
+        (context.resources.getInteger(R.integer.BookCoverSize) + context.resources.getInteger(R.integer.BookCoverAdditionalTileEnlargement)).dp
+    }
+
+
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
+        columns = GridCells.Adaptive(minSize = tileSizeInDp),
         contentPadding = PaddingValues(8.dp)
     ) {
         items(entries.size) { index ->
@@ -52,13 +62,25 @@ fun DataEntryGrid(entries: List<DataEntry>) {
 
 @Composable
 fun DataEntryTile(entry: DataEntry) {
+    val context = LocalContext.current
+    val bookCoverSizeInDp = with(LocalDensity.current) {
+        context.resources.getInteger(R.integer.BookCoverSize).dp
+    }
+    val outerCornerClipInDp = with(LocalDensity.current) {
+        context.resources.getInteger(R.integer.OuterCornerClip).dp
+    }
+    val innerCornerClipInDp = with(LocalDensity.current) {
+        context.resources.getInteger(R.integer.InnerCornerClip).dp
+    }
+
     Box(
         modifier = Modifier
             .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(outerCornerClipInDp))
             .background(OverlayBackgroundGrayHalfTransparent)
             .fillMaxWidth()
-            .wrapContentHeight()
+            .wrapContentHeight(),
+        contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier.padding(8.dp),
@@ -68,9 +90,8 @@ fun DataEntryTile(entry: DataEntry) {
                 painter = painterResource(id = entry.imageResId),
                 contentDescription = entry.Title,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .clip(RoundedCornerShape(12.dp))
+                    .width(bookCoverSizeInDp)
+                    .clip(RoundedCornerShape(innerCornerClipInDp))
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
